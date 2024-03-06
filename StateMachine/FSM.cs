f3c82555ec37b7210b68a0e10fbaca88d215cb28
Daniil Katkov <58704183+Sulz759@users.noreply.github.com>
2024-03-07 00:28:34 +0300

    Added the first patterns

---
+++ StateMachine/FSM.cs
@@ -0,0 +1,39 @@
+﻿namespace StateMachine
+{
+	public class FSM
+	{
+		public FSMState StateCurrent {get;private set;}
+
+		private Dictionary<Type, FSMState> _states = new Dictionary<Type, FSMState>();
+
+		public void AddState(FSMState state) 
+		{
+			_states.Add(state.GetType(), state);
+		}
+
+		public void SetState<T>() where T : FSMState 
+		{
+			var type = typeof(T);
+
+			if (StateCurrent != null && StateCurrent.GetType() == type) 
+			{
+				return;
+			}
+
+			if (_states.TryGetValue(type, out var newState)) 
+			{
+				StateCurrent?.Exit();
+
+				StateCurrent = newState;
+
+				StateCurrent.Enter();
+			}
+		}
+
+		public void Update() 
+		{ 
+			StateCurrent?.Update();
+		}
+	}
+
+}
---
+++ StateMachine/FSMState.cs
@@ -0,0 +1,21 @@
+﻿namespace StateMachine
+{
+	public abstract class FSMState
+	{
+		protected readonly FSM Fsm;
+
+		public FSMState(FSM fsm)
+		{
+			Fsm = fsm;
+		}
+
+		protected FSMState()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public virtual void Enter() { }
+		public virtual void Exit() { }
+		public virtual void Update() { }
+	}
+}
